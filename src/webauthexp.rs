db45ba972eea12b1455d58c8ececb4f7a7f069e0
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-02 21:50:19 +0900

    Enable to run server

--- src/webauthexp.rs
+++ src/webauthexp.rs
@@ -1,7 +1,12 @@
+use actix_web::{App, HttpServer};
 use anyhow::Result;
-use tokio;
 
-#[tokio::main]
+#[actix_rt::main]
 async fn main() -> Result<()> {
+    let server = HttpServer::new(move || {
+        App::new()
+    });
+    server.bind("127.0.0.1:8080")?.run().await?;
+
     Ok(())
 }
