c3d90da46d5de6363fd27a2301ba612100f36f63
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-02 22:11:14 +0900

    Enable to put logs

--- src/webauthexp.rs
+++ src/webauthexp.rs
@@ -1,5 +1,6 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 use anyhow::Result;
+use env_logger::Env;
 
 mod app;
 use crate::app::config::AppConfig;
@@ -8,8 +9,12 @@ use crate::app::config::AppConfig;
 async fn main() -> Result<()> {
     let config = AppConfig::new();
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let server = HttpServer::new(move || {
         App::new()
+        .wrap(Logger::default())
+        .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
     });
     server.bind(config.bind_address())?.run().await?;
 
