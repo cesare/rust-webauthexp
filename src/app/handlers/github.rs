84e2ebdb96c81a550a073a6e297f8a92f81c6b5d
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-05 21:31:59 +0900

    Add an empty endpoint

---
+++ src/app/handlers/github.rs
@@ -0,0 +1,8 @@
+use actix_http::body::Body;
+use actix_web::{get, HttpResponse, Result};
+
+#[get("/github")]
+pub async fn request_authorization() -> Result<HttpResponse<Body>> {
+    let response = HttpResponse::Ok().body("ok");
+    Ok(response)
+}
--- src/webauthexp.rs
+++ src/webauthexp.rs
@@ -4,6 +4,7 @@ use app::config::AppArgs;
 use env_logger::Env;
 
 mod app;
+use crate::app::handlers::{self};
 
 #[actix_rt::main]
 async fn main() -> Result<()> {
@@ -16,6 +17,7 @@ async fn main() -> Result<()> {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
+            .service(handlers::github::request_authorization)
     });
     server.bind(config.bind_address())?.run().await?;
 
