21995716d78a0a44b2762c356a359097954bd835
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-07 22:22:02 +0900

    Pass configuration to handlers

--- src/bin/webauthexp.rs
+++ src/bin/webauthexp.rs
@@ -13,14 +13,16 @@ async fn main() -> Result<()> {
 
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
+    let bind_address = config.bind_address();
     let server = HttpServer::new(move || {
         App::new()
+            .data(config.clone())
             .wrap(Logger::default())
             .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
             .wrap(CookieSession::signed(&[0; 32]).secure(false))
             .service(handlers::github::request_authorization)
     });
-    server.bind(config.bind_address())?.run().await?;
+    server.bind(bind_address)?.run().await?;
 
     Ok(())
 }
