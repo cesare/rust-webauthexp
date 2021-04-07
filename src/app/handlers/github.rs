d52209272bc254a14e0fca5c144894e648e9bf56
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-07 22:42:37 +0900

    Enable handler to receive configuration

--- src/app/handlers/github.rs
+++ src/app/handlers/github.rs
@@ -2,8 +2,10 @@ use actix_http::body::Body;
 use actix_session::Session;
 use actix_web::{get, HttpResponse, Result};
 
+use crate::app::config::AppConfig;
+
 #[get("/github")]
-pub async fn request_authorization(_session: Session) -> Result<HttpResponse<Body>> {
+pub async fn request_authorization(_data: actix_web::web::Data<AppConfig>, _session: Session) -> Result<HttpResponse<Body>> {
     let response = HttpResponse::Ok().body("ok");
     Ok(response)
 }
