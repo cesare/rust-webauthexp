b480d9aed44106b9137ab9aa6c55786e7b97f489
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-08 23:00:55 +0900

    Use ServiceConfig

--- src/app/handlers/github.rs
+++ src/app/handlers/github.rs
@@ -1,10 +1,16 @@
 use actix_http::body::Body;
 use actix_session::Session;
-use actix_web::{get, HttpResponse, Result};
+use actix_web::{HttpResponse, Result, web::{get, resource, ServiceConfig}};
 
 use crate::app::config::AppConfig;
 
-#[get("/github")]
+pub fn config(cfg: &mut ServiceConfig) {
+    cfg.service(
+        resource("/github")
+            .route(get().to(index))
+    );
+}
+
 pub async fn index(_data: actix_web::web::Data<AppConfig>, _session: Session) -> Result<HttpResponse<Body>> {
     let response = HttpResponse::Ok().body("ok");
     Ok(response)
