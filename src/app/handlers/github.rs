e872b1aee3651e395ab51f303e38222f0ce4993a
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-09 21:54:35 +0900

    Use scope

--- src/app/handlers/github.rs
+++ src/app/handlers/github.rs
@@ -1,13 +1,14 @@
 use actix_http::body::Body;
 use actix_session::Session;
-use actix_web::{HttpResponse, Resource, Result, web::{get, resource, Data}};
+use actix_web::{HttpResponse, Result, Scope, web::{Data, get, scope}};
 
 use crate::app::config::{AppConfig, GithubConfig};
 
-pub fn create_resource(config: &AppConfig) -> Resource {
-    resource("/github")
+pub fn create_scope(config: &AppConfig) -> Scope {
+    scope("/github")
         .data(config.github.clone())
-        .route(get().to(index))
+        .route("", get().to(index))
+        .route("/", get().to(index))
 }
 
 async fn index(_data: Data<GithubConfig>, _session: Session) -> Result<HttpResponse<Body>> {
--- src/bin/webauthexp.rs
+++ src/bin/webauthexp.rs
@@ -19,7 +19,7 @@ async fn main() -> Result<()> {
             .wrap(Logger::default())
             .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
             .wrap(CookieSession::signed(&[0; 32]).secure(false))
-            .service(handlers::github::create_resource(&config))
+            .service(handlers::github::create_scope(&config))
     });
     server.bind(bind_address)?.run().await?;
 
