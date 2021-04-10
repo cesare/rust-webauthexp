711ebc37c2717bc8458a07b19d2802d28eedd3f8
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-10 22:48:04 +0900

    Enable to request authorization

--- src/app/handlers/github.rs
+++ src/app/handlers/github.rs
@@ -10,11 +10,19 @@ pub fn create_scope(config: &AppConfig) -> Scope {
         .data(config.github.clone())
         .route("", get().to(index))
         .route("/", get().to(index))
+        .route("/callback", get().to(callback))
 }
 
 async fn index(data: Data<GithubConfig>, _session: Session) -> Result<HttpResponse<Body>> {
     let request = GithubAutorizationRequest::new(&data);
     let request_uri = request.request_uri().unwrap();
-    let response = HttpResponse::Ok().body(request_uri);
+    let response = HttpResponse::Found()
+        .insert_header(("Location", request_uri))
+        .finish();
+    Ok(response)
+}
+
+async fn callback(_data: Data<GithubConfig>, _session: Session) -> Result<HttpResponse<Body>> {
+    let response = HttpResponse::Ok().body("ok");
     Ok(response)
 }
