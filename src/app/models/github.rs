b3273cf9cd9023ed66838b8d00f175e6d7e9a0a8
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-18 15:36:45 +0900

    Use lifetime to hold the reference

--- src/app/models/github.rs
+++ src/app/models/github.rs
@@ -7,34 +7,31 @@ use url::Url;
 
 use crate::app::config::GithubConfig;
 
-pub struct GithubAutorizationRequest {
-    client_id: String,
-    redirect_uri: String,
-    scope: String,
+pub struct GithubAutorizationRequest<'a> {
+    config: &'a GithubConfig,
     pub state: String,
 }
 
-impl GithubAutorizationRequest {
-    pub fn new(config: &GithubConfig) -> Self {
+impl<'a> GithubAutorizationRequest<'a> {
+    pub fn new(config: &'a GithubConfig) -> Self {
         let mut rng = StdRng::from_entropy();
         let mut rs: [u8; 32] = [0; 32];
         rng.fill_bytes(&mut rs);
         let state = base64::encode_config(rs, base64::URL_SAFE);
 
         Self {
-            client_id: config.client_id.to_owned(),
-            redirect_uri: config.redirect_uri.to_owned(),
-            scope: config.scope.to_owned(),
+            config: config,
             state: state,
         }
     }
 
     pub fn request_uri(&self) -> Result<String> {
+        let config = self.config;
         let base = "https://github.com/login/oauth/authorize";
         let parameters = vec![
-            ("client_id", &self.client_id),
-            ("redirect_uri", &self.redirect_uri),
-            ("scope", &self.scope),
+            ("client_id", &config.client_id),
+            ("redirect_uri", &config.redirect_uri),
+            ("scope", &config.scope),
             ("state", &self.state),
         ];
         let url = Url::parse_with_params(base, &parameters)?;
