fc4923fe7be0ef27a8a2c02640d6d5c7ffde8e28
SAWADA Tadashi <cesare@mayverse.jp>
2021-04-26 22:58:44 +0900

    Add Jws

--- src/app/models/jwt.rs
+++ src/app/models/jwt.rs
@@ -1,6 +1,41 @@
 use serde_derive::Deserialize;
 use thiserror::Error;
 
+pub struct Jws {
+    pub header: Header,
+    pub payload: Vec<u8>,
+    pub signature: Vec<u8>,
+}
+
+pub struct JwsDecoder {
+}
+
+impl JwsDecoder {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn execute(&self, token_string: &str) -> Result<Jws, IdTokenDecodingError> {
+        let parts: Vec<&str> = token_string.splitn(3, ".").collect();
+        if parts.len() != 3 {
+            return Err(IdTokenDecodingError::InvalidFormat)
+        }
+
+        let binary_header = base64::decode(parts[0])?;
+        let payload = base64::decode(parts[1])?;
+        let signature = base64::decode(parts[2])?;
+
+        let header = serde_json::from_slice::<Header>(binary_header.as_ref())?;
+
+        let jws = Jws {
+            header: header,
+            payload: payload,
+            signature: signature,
+        };
+        Ok(jws)
+    }
+}
+
 pub struct IdToken {
     pub header: Header,
     pub claims: Claims,
